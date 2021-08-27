78356d3bba35c1e43dee2b2c1fd9ccbf785a34aa
Geverson Souza <geversonjosedesouza@gmail.com>
2021-08-27 19:18:36 -0300

    Add files via upload

---
+++ Aula_20210827.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    
+    float total, num;
+    int jabuticaba;
+    total = jabuticaba + num;
+    printf("A soma entre %d e %f = %f", jabuticaba, num, total);
+    //cout<<"Hello World";
+
+    return 0;
+}
