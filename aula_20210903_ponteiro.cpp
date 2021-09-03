b0c29a9fc5b665fe355ef4fa82aee3aceab7bffd
Geverson Souza <geversonjosedesouza@gmail.com>
2021-09-03 19:38:15 -0300

    Update aula_20210903_ponteiro.cpp

--- aula_20210903_ponteiro.cpp
+++ aula_20210903_ponteiro.cpp
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+
+int main(){
+    
+    int x = 10;
+    int *p;
+    
+    p = &x;
+    
+    printf("\tAntes\n");
+    printf("Variável x = %d\n", x);
+    printf("Valor do ponteiro p = %d\n", p);
+    printf("O endereço do ponteiro *p = %d\n", *p);
+    
+    *p=15;
+    
+    printf("\tDepois\n");
+    printf("Variável x = %d\n", x);
+    printf("Valor do ponteiro p = %d\n", p);
+    printf("O endereço do ponteiro *p = %d\n", *p);
+    
+    return 0;
+}
