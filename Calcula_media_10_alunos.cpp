abc52739c8ec88ab0bcf8d7c7ad9c98a5fb8ca63
Geverson Souza <geversonjosedesouza@gmail.com>
2021-09-03 19:05:05 -0300

    Update Calcula_media_10_alunos.cpp

--- Calcula_media_10_alunos.cpp
+++ Calcula_media_10_alunos.cpp
@@ -1,26 +1,24 @@
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
 int main(){
     
-    float notas[10], nota;
-    char nomes[10], nome;
+    float notas[10];
+    char nomes[10][30];
     
     for(int i = 0; i < 10; i++){
-        printf("\nDigite o nome do %dº aluno: ",i);
-        scanf("%c", &nomes[i]);
+        printf("\nDigite o nome do aluno: ");
+        scanf("%s", &nomes[i][30]);
     
-        printf("\nDigite a nota do aluno %c: ", nomes[i]);
-        scanf("%.2f", &nota);
-        notas[i] = nota;
+        printf("\nDigite a nota do aluno: ");
+        scanf("%f", &notas[i]);
     }
     
     for(int i = 0; i < 10; i++){
         if(notas[i] >= 7){
-            printf("\nO Aluno %c foi aprovado com nota %.2f: ", nomes[i], notas[i]);
+            printf("\nO Aluno %c foi aprovado com nota %.2f: ", nomes[i][30], notas[i]);
         }else{
-            printf("\nO Aluno %c foi reprovado com nota %.2f: ", nomes[i], notas[i]);
+            printf("\nO Aluno %c foi reprovado com nota %.2f: ", nomes[i][30], notas[i]);
         }
     }
     return 0;
