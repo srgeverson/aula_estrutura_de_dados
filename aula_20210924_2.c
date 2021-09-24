f832ce3f74358ae19313e7aa776924e765f5ca19
Geverson Souza <geversonjosedesouza@gmail.com>
2021-09-24 20:27:13 -0300

    Create aula_20210924_2.c

---
+++ aula_20210924_2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* retorna_ponteiro(int tamanho){
+   int *vetor = (int *) calloc(tamanho, sizeof(int));
+    for(int i = 0; i < tamanho; i++){
+        printf("Informe o %dº elemento: ", i+1);
+        scanf("%d", &vetor[i]);
+    }
+    return vetor;
+}
+
+int* imprime_vetor(int *vetor, int tamanho){
+    for(int i = 0; i < tamanho; i++){
+        if(i == tamanho-1)
+         printf("%d", vetor[i]);
+        else
+         printf("%d,", vetor[i]);
+    }
+    return vetor;    
+}
+
+void libera_memoria(int *vetor){
+    printf("\nLimpando memória!");
+    free(vetor);
+}
+
+int main(){
+    
+    int n;
+    printf("Qual o tamanho do vetor que deseja: ");
+    scanf("%d", &n);
+    
+   libera_memoria(imprime_vetor(retorna_ponteiro(n), n));
+        
+    return 0;
+}
