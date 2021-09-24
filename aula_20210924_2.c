51d16477cd8b9737eb9741392a214462c25d57f7
Geverson Souza <geversonjosedesouza@gmail.com>
2021-09-24 20:30:56 -0300

    Update aula_20210924_2.c

--- aula_20210924_2.c
+++ aula_20210924_2.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 
 int* retorna_ponteiro(int tamanho){
-   int *vetor = (int *) calloc(tamanho, sizeof(int));
+    printf("\nPreenchendo a lista de itens!\n");
+    int *vetor = (int *) calloc(tamanho, sizeof(int));
     for(int i = 0; i < tamanho; i++){
         printf("Informe o %dº elemento: ", i+1);
         scanf("%d", &vetor[i]);
@@ -11,6 +12,7 @@ int* retorna_ponteiro(int tamanho){
 }
 
 int* imprime_vetor(int *vetor, int tamanho){
+    printf("\nImprimindo itens!\n");
     for(int i = 0; i < tamanho; i++){
         if(i == tamanho-1)
          printf("%d", vetor[i]);
@@ -26,12 +28,12 @@ void libera_memoria(int *vetor){
 }
 
 int main(){
-    
     int n;
+    
     printf("Qual o tamanho do vetor que deseja: ");
     scanf("%d", &n);
     
-   libera_memoria(imprime_vetor(retorna_ponteiro(n), n));
+    libera_memoria(imprime_vetor(retorna_ponteiro(n), n));
         
     return 0;
 }
