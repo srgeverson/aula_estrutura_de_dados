b96a60d42372fcc5c4c5f6094f7bf2fe868afeb1
Geverson Souza <geversonjosedesouza@gmail.com>
2021-11-08 21:27:31 -0300

    Rename exercicio03.c to exercicio03_AV1.c

--- exercicio03_AV1.c
+++ exercicio03_AV1.c
@@ -12,11 +12,11 @@ void invertePalavras (char *parametro1, char *parametro2) {
 
 int main () { 
 	char primeiraPalavra[STRMAX], segundaPalavra[STRMAX]; 
-	printf ("Digite uma palavra com no m·ximo 20 letras: "); 
+	printf ("Digite uma palavra com no m√°ximo 20 letras: "); 
 	gets (primeiraPalavra); 
-	printf ("Digite outra palavra com no m·ximo 20 letras: ");
+	printf ("Digite outra palavra com no m√°ximo 20 letras: ");
 	gets (segundaPalavra); 
-	printf ("\nAntes da invers„o a primeira palavra = %s e segunda palavra = %s", primeiraPalavra, segundaPalavra);
+	printf ("\nAntes da invers√£o a primeira palavra = %s e segunda palavra = %s", primeiraPalavra, segundaPalavra);
 	invertePalavras(primeiraPalavra, segundaPalavra); 
-	printf ("\nDepois da invers„o a primeira palavra = %s e segunda palavra = %s", primeiraPalavra, segundaPalavra);
+	printf ("\nDepois da invers√£o a primeira palavra = %s e segunda palavra = %s", primeiraPalavra, segundaPalavra);
 }
